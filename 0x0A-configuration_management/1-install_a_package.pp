096946e81dc023ed09e8cc2dce0ee62b3c2c7e1c
Ezralx <ezrateshome.alx@gmail.com>
2024-05-07 23:00:30 +0300

    feat:Using Puppet, install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,14 @@
 #Install flask version 2.1.0 and Werkzeug 2.1.1
 
 package{'flask Installation':
-    name => flask,
-    ensure => '2.1.0',
+    ensure   => '2.1.0',
+    name     => flask,
     provider => 'pip3',
 
 }
 package{'Werkzeug Installation':
-    name => werkzeug,
-    ensure => '2.1.1',
+    ensure   => '2.1.1',
+    name     => werkzeug,
     provider => 'pip3',
 
 }
